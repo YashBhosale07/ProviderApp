bd6e4ff47f87bda24a3367b98a5cc4139c3d70a8
YashBhosale07 <yashbhosale833@gmail.com>
2024-10-14 15:42:18 +0530

    add get post

--- SpringBoot-ProviderApp/src/main/java/com/providerApp/RestController/WishMessgaeController.java
+++ SpringBoot-ProviderApp/src/main/java/com/providerApp/RestController/WishMessgaeController.java
@@ -5,8 +5,13 @@ import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.providerApp.model.Customer;
 
 @RestController
 @RequestMapping("/providerApp")
@@ -27,5 +32,21 @@ public class WishMessgaeController {
 			Body="Good Night";
 		}
 		return new ResponseEntity<String>(Body,HttpStatus.OK);
+	}
+	
+	@GetMapping("/welcome/{id}/{name}")
+	public ResponseEntity<String>welcomeMessage(@PathVariable int id,@PathVariable String name){
+		return new ResponseEntity<String>("welcome "+id+" "+name,HttpStatus.ACCEPTED);
+	}
+	
+	@PostMapping("/saveCustomer")
+	public ResponseEntity<String>register(@RequestBody Customer c){
+		return new ResponseEntity<String>(c.toString(),HttpStatus.OK);
+	}
+	
+	@GetMapping("/findById/{id}")
+	public ResponseEntity<Customer>findRecord(@PathVariable int id){
+		Customer c=new Customer(id,"Thala","Ranchi");
+		return new ResponseEntity<Customer>(c,HttpStatus.FOUND);
 	}
 }
